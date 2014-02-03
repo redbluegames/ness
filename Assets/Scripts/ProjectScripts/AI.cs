64edfac61d2ca2de029821662a622cc71498e891
Edward Rowe <rowedrage@gmail.com>
2014-02-03 17:01:18 -0500

    Added animation controlled root movement to CharacterMotor.
    I added an attack to Rats to test animation driven root
    movement. Reworked the AI and Enemy to handle the new
    motor.

--- Assets/Scripts/ProjectScripts/AI.cs
+++ Assets/Scripts/ProjectScripts/AI.cs
@@ -4,75 +4,41 @@ using System.Collections;
 public class AI : MonoBehaviour
 {
 
-	public GameObject Target;
-	public CharacterMotor motor;
-	public Animation attackAnimation;
-	public AttackCast attackCaster;
-	CountDownTimer attackTime = new CountDownTimer ();
+	public GameObject Target {get; private set;}
+	public Enemy enemy;
 	CountDownTimer attackCooldown = new CountDownTimer ();
 	bool isAttacking;
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
 		FindTarget ();
 
 		if (Target != null) {
+			isAttacking = enemy.isAttacking;
 			float sqrDistanceToTarget = (Target.transform.position - transform.position).sqrMagnitude;
-			float ATTACK_RANGE_SQUARED = 6.0f;
-			bool isInAttackRange = sqrDistanceToTarget > ATTACK_RANGE_SQUARED;
-			if (isInAttackRange && !isAttacking) {
-				// Approach target until in range
-				motor.MoveDirection = Target.transform.position - transform.position;
+			float ATTACK_RANGE_SQUARED = 16.0f;
+			bool isInAttackRange = sqrDistanceToTarget <= ATTACK_RANGE_SQUARED;
+			if (isInAttackRange && attackCooldown.IsTimeUp()) {
+				enemy.wantsToAttack = true;
+				attackCooldown.StartTimer (2.0f);
 			} else {
-				// Attack
-				// Stop moving
-				motor.MoveDirection = Vector3.zero;
-
-				// Begin or end a running attack
-				if (!isAttacking && attackCooldown.IsTimeUp ()) {
-					StartAttack ();
-				} else if (isAttacking && attackTime.IsTimeUp ()) {
-					EndAttack ();
+				if(isInAttackRange && !isAttacking) {
+					// Stop moving
+					enemy.moveDirection = Vector3.zero;
+				} else {
+					// Approach target until in range. Also approach during attack
+					enemy.moveDirection = (Target.transform.position - transform.position);
 				}
-
-				// Wait for cooldown
 			}
 
 			// Always face the target
-			motor.FaceDirection = Target.transform.position - transform.position;
+			enemy.faceDirection =  ( Target.transform.position - transform.position);
 		}
-	}
-
-	void StartAttack ()
-	{
-		attackAnimation.Play ();
-		attackCaster.OnHit += OnAttackHit;
-		attackCaster.Begin ();
-		attackTime.StartTimer (attackAnimation.clip.length);
-		isAttacking = true;
-	}
-
-	void EndAttack ()
-	{
-		attackCooldown.StartTimer (1.0f);
-		attackCaster.End ();
-		isAttacking = false;
 	}
 	
 	void FindTarget ()
 	{
 		Target = GameObject.Find (ObjectNames.PLAYER);
-	}
-
-	/*
-	 * Deal damage to the hit object based on the current attack.
-	 */
-	void OnAttackHit (RaycastHit hit)
-	{
-		Damage damageOut = new Damage (10.0f, transform);
-		GameObject hitGameObject = hit.transform.gameObject;
-		hitGameObject.SendMessage ("ApplyDamage", damageOut, SendMessageOptions.DontRequireReceiver);
 	}
 }
