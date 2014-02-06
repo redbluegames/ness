af1683a278e26b5d3fae968847adbfd8021c6293
LucasRowe <lucas.rowe@gmail.com>
2014-02-06 18:19:17 -0500

    Small water pool added for fun.

---
+++ Assets/Scripts/RedBlueScripts/TweenScrollTexture.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Move a texture across a plane at a given linear speed.
+/// </summary>
+public class TweenScrollTexture : MonoBehaviour
+{
+	public Vector2 speed;
+	
+	void Update ()
+	{
+		renderer.material.mainTextureOffset = Time.time * speed;
+	}
+
+}
