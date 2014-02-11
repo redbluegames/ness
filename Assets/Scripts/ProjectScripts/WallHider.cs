3572480ce8c2709d8825fdeed477c9e284836a5b
LucasRowe <lucas.rowe@gmail.com>
2014-02-11 16:22:50 -0500

    Refactored TweenFade to allow any target alpha and always work!

--- Assets/Scripts/ProjectScripts/WallHider.cs
+++ Assets/Scripts/ProjectScripts/WallHider.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 
 public class WallHider : MonoBehaviour
 {
-	const float HIDESPEED = 0.3f;
+	const float HIDESPEED = 0.25f;
 	readonly Vector3 CASTOFFSET = new Vector3 (0, -0.2f, 0);
-	List<GameObject> previouslyHidden = new List<GameObject> ();
+	List<GameObject> previouslyHiddenWalls = new List<GameObject> ();
 
 	// Cached References
 	Transform playerTransform;
@@ -28,39 +28,47 @@ public class WallHider : MonoBehaviour
 		//Debug.DrawRay (cameraTransform.position, toPlayer, Color.red);
 		int onlyTempWalls = 1 << Layers.TEMP_WALL;
 		RaycastHit[] hits = Physics.RaycastAll (camToPlayer, 25.0f, onlyTempWalls);
-		for (int i = 0; i < hits.Length; ++i) {
-			TweenFadeInOut fader = hits [i].collider.gameObject.GetComponent<TweenFadeInOut> ();
-			if (fader == null) {
-				Debug.LogWarning ("Encountered a temporary wall without a fader script at " +
-					"position " + hits [i].point);
-				continue;
-			}
-			if (hits [i].point.z < playerTransform.position.z) {
-				fader.FadeOut (HIDESPEED);
-				previouslyHidden.Add (hits [i].collider.gameObject);
-			}
-		}
+		FadeObstructingWallsOut (hits);
+		FadeUnobstructingWallsIn (hits);
+	}
 
-		for (int i = previouslyHidden.Count-1; i >= 0; --i) {
-			bool containsWall = false;
-			foreach (RaycastHit hit in hits) {
-				if (previouslyHidden [i].Equals (hit.collider.gameObject)) {
-					containsWall = true;
-				}
-			}
-			if (!containsWall) {
-				TweenFadeInOut fader = previouslyHidden [i].GetComponent<TweenFadeInOut> ();
-				fader.FadeIn (HIDESPEED);
-				previouslyHidden.Remove (previouslyHidden [i]);
+	/// <summary>
+	/// Fades all walls out that were hit in a provided raycast.
+	/// </summary>
+	/// <param name="obstructingWalls">Walls hit by a raycast from the camera towards the player.</param>
+	void FadeObstructingWallsOut (RaycastHit[] obstructingWalls)
+	{
+		for (int i = 0; i < obstructingWalls.Length; ++i) {
+			TweenFadeInOut fader = obstructingWalls [i].collider.gameObject.GetComponent<TweenFadeInOut> ();
+			if (fader != null) {
+				fader.FadeOut (HIDESPEED);
+				previouslyHiddenWalls.Add (obstructingWalls [i].collider.gameObject);
+			} else {
+				Debug.LogWarning ("Encountered a temporary wall without a fader script at " +
+					"position " + obstructingWalls [i].point);
 			}
 		}
 	}
 
-	IEnumerator FadeWallIn (TweenFadeInOut fader)
+	/// <summary>
+	/// Fades the walls in if they are no longer in the list of walls hit by
+	/// our camera's raycast.
+	/// </summary>
+	/// <param name="obstructingWalls">Walls hit by a raycast from the camera towards the player.</param>
+	void FadeUnobstructingWallsIn (RaycastHit[] obstructingWalls)
 	{
-		while (fader.isFadingIn) {
-			yield return null;
+		for (int i = previouslyHiddenWalls.Count-1; i >= 0; --i) {
+			bool wallStillObstructing = false;
+			for (int h = 0; h < obstructingWalls.Length; ++h) {
+				if (previouslyHiddenWalls [i].Equals (obstructingWalls [h].collider.gameObject)) {
+					wallStillObstructing = true;
+				}
+			}
+			if (!wallStillObstructing) {
+				TweenFadeInOut fader = previouslyHiddenWalls [i].GetComponent<TweenFadeInOut> ();
+				fader.FadeIn (HIDESPEED);
+				previouslyHiddenWalls.Remove (previouslyHiddenWalls [i]);
+			}
 		}
-		fader.FadeIn (HIDESPEED);
 	}
 }
--- Assets/Scripts/RedBlueScripts/TweenFadeInOut.cs
+++ Assets/Scripts/RedBlueScripts/TweenFadeInOut.cs
@@ -4,64 +4,90 @@ using System.Collections;
 [RequireComponent (typeof(Renderer))]
 public class TweenFadeInOut : MonoBehaviour
 {
-	public float fadeTime = 0.5f;
-	public bool isFadingOut { get; private set; }
-	public bool isFadingIn { get; private set; }
+	float DEFAULT_FADE_TIME = 0.0f;
+	float fadeTime = 0.5f;
+	float targetAlpha = 1.0f;
+	public bool isFading { get; private set; }
 
-	public void FadeOut ()
-	{
-		FadeOut (fadeTime);
-	}
-
-	public void FadeOut (float time)
-	{
-		// If fading in, stop it and fade out, if already fading out, do nothing
-		if (isFadingOut) {
-			return;
-		}
-		if (isFadingIn) {
-			StopCoroutine ("Fade");
-			isFadingIn = false;
-		}
-		isFadingOut = true;
-		fadeTime = time;
-		StartCoroutine ("Fade", 0);
-	}
-
+	/// <summary>
+	/// Fade the object in at the default speed.
+	/// </summary>
 	public void FadeIn ()
 	{
-		FadeIn (fadeTime);
+		FadeIn (DEFAULT_FADE_TIME);
 	}
 
-	public void FadeIn (float time)
+	/// <summary>
+	/// Fades the object in, taking a provided amount of time to fade.
+	/// </summary>
+	/// <param name="timeToFade">Time to fade.</param>
+	public void FadeIn (float timeToFade)
 	{
-		// If fading out, stop it and fade in, if already fading in, do nothing
-		if (isFadingIn) {
-			return;
-		}
-		if (isFadingOut) {
-			StopCoroutine ("Fade");
-			isFadingOut = false;
-		}
-		isFadingIn = true;
-		fadeTime = time;
-		StartCoroutine ("Fade", 1);
+		SetNewFadeTarget (1.0f, timeToFade);
+	}
+	
+	/// <summary>
+	/// Fade the object out at the default speed.
+	/// </summary>
+	public void FadeOut ()
+	{
+		FadeOut (DEFAULT_FADE_TIME);
 	}
 
-	IEnumerator Fade (float alphaValue)
+	/// <summary>
+	/// Fades the object out, taking a provided amount of time to fade.
+	/// </summary>
+	/// <param name="timeToFade">Time to fade.</param>
+	public void FadeOut (float timeToFade)
 	{
+		SetNewFadeTarget (0.0f, timeToFade);
+	}
+
+	/// <summary>
+	/// Fade to a provided alpha and over a provided amount of time.
+	/// </summary>
+	/// <param name="targetAlpha">Target alpha.</param>
+	/// <param name="fadeTime">Fade time.</param>
+	void SetNewFadeTarget (float targetAlpha, float fadeTime)
+	{
+		this.fadeTime = fadeTime;
+		this.targetAlpha = targetAlpha;
+
+		// Start the corountine if we need to fade to reach the new target and it's not already running.
+		if (!IsAlphaAtTarget () && !isFading) {
+			StartCoroutine (Fade ());
+		}
+	}
+
+	/// <summary>
+	/// Fade to the current target. This method can only be running once but the
+	/// target and time are allowed to change out from under it.
+	/// </summary>
+	IEnumerator Fade ()
+	{
+		isFading = true;
 		Color currentColor = gameObject.renderer.material.color;
-		Color targetColor = gameObject.renderer.material.color;
-		targetColor.a = alphaValue;
+		
+		while (!IsAlphaAtTarget()) {
+			// Recalculate the rate of change in case targets change mid fade
+			float alphaRemaining = targetAlpha - currentColor.a;
+			float MAX_ALPHA = 1.0f;
+			float rate = (MAX_ALPHA / fadeTime) * Mathf.Sign (alphaRemaining);
 
-		// Fast-forward if the fade is midway through
-		float currentFadeProgress = Mathf.Abs (alphaValue - currentColor.a);
-		float elapsed = Mathf.Max (0, fadeTime - (currentFadeProgress * fadeTime));
-		while (elapsed < fadeTime) {
-			elapsed += Time.deltaTime;
-			gameObject.renderer.material.color = Color.Lerp (currentColor, targetColor, elapsed / fadeTime);
+			currentColor.a = Mathf.Clamp01 (currentColor.a + rate * Time.deltaTime);
+			gameObject.renderer.material.color = currentColor;
 			yield return null;
 		}
+		isFading = false;
+	}
+
+	/// <summary>
+	/// Determines whether the gameobject's alpha is at target value.
+	/// </summary>
+	/// <returns><c>true</c> if this instance is alpha at target; otherwise, <c>false</c>.</returns>
+	private bool IsAlphaAtTarget ()
+	{
+		return Mathf.Approximately (gameObject.renderer.material.color.a, targetAlpha);
 	}
 
 }
