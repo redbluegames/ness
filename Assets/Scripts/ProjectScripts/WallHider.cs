78468770ceb0def960db0b24de3cec31b1cdb36e
LucasRowe <lucas.rowe@gmail.com>
2014-02-11 14:30:50 -0500

    Walls now can fade.
    Tween fading utility added.
    Script to hide walls added.
    Prefabs modified.
    Layer for temporary walls added.

---
+++ Assets/Scripts/ProjectScripts/WallHider.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WallHider : MonoBehaviour
+{
+	const float HIDESPEED = 0.3f;
+	readonly Vector3 CASTOFFSET = new Vector3 (0, -0.2f, 0);
+	List<GameObject> previouslyHidden = new List<GameObject> ();
+
+	// Cached References
+	Transform playerTransform;
+	Transform cameraTransform;
+
+	void Awake ()
+	{
+		GameObject player = GameObject.Find (ObjectNames.PLAYER);
+		playerTransform = player.transform;
+		cameraTransform = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		Vector3 toPlayer = playerTransform.position - cameraTransform.position + 
+			cameraTransform.TransformDirection (CASTOFFSET);
+		Ray camToPlayer = new Ray (cameraTransform.position, toPlayer);
+		//Debug.DrawRay (cameraTransform.position, toPlayer, Color.red);
+		int onlyTempWalls = 1 << Layers.TEMP_WALL;
+		RaycastHit[] hits = Physics.RaycastAll (camToPlayer, 25.0f, onlyTempWalls);
+		for (int i = 0; i < hits.Length; ++i) {
+			TweenFadeInOut fader = hits [i].collider.gameObject.GetComponent<TweenFadeInOut> ();
+			if (fader == null) {
+				Debug.LogWarning ("Encountered a temporary wall without a fader script at " +
+					"position " + hits [i].point);
+				continue;
+			}
+			if (hits [i].point.z < playerTransform.position.z) {
+				fader.FadeOut (HIDESPEED);
+				previouslyHidden.Add (hits [i].collider.gameObject);
+			}
+		}
+
+		for (int i = previouslyHidden.Count-1; i >= 0; --i) {
+			bool containsWall = false;
+			foreach (RaycastHit hit in hits) {
+				if (previouslyHidden [i].Equals (hit.collider.gameObject)) {
+					containsWall = true;
+				}
+			}
+			if (!containsWall) {
+				TweenFadeInOut fader = previouslyHidden [i].GetComponent<TweenFadeInOut> ();
+				fader.FadeIn (HIDESPEED);
+				previouslyHidden.Remove (previouslyHidden [i]);
+			}
+		}
+	}
+
+	IEnumerator FadeWallIn (TweenFadeInOut fader)
+	{
+		while (fader.isFadingIn) {
+			yield return null;
+		}
+		fader.FadeIn (HIDESPEED);
+	}
+}
---
+++ Assets/Scripts/RedBlueScripts/TweenFadeInOut.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Renderer))]
+public class TweenFadeInOut : MonoBehaviour
+{
+	public float fadeTime = 0.5f;
+	public bool isFadingOut { get; private set; }
+	public bool isFadingIn { get; private set; }
+
+	public void FadeOut ()
+	{
+		FadeOut (fadeTime);
+	}
+
+	public void FadeOut (float time)
+	{
+		// If fading in, stop it and fade out, if already fading out, do nothing
+		if (isFadingOut) {
+			return;
+		}
+		if (isFadingIn) {
+			StopCoroutine ("Fade");
+			isFadingIn = false;
+		}
+		isFadingOut = true;
+		fadeTime = time;
+		StartCoroutine ("Fade", 0);
+	}
+
+	public void FadeIn ()
+	{
+		FadeIn (fadeTime);
+	}
+
+	public void FadeIn (float time)
+	{
+		// If fading out, stop it and fade in, if already fading in, do nothing
+		if (isFadingIn) {
+			return;
+		}
+		if (isFadingOut) {
+			StopCoroutine ("Fade");
+			isFadingOut = false;
+		}
+		isFadingIn = true;
+		fadeTime = time;
+		StartCoroutine ("Fade", 1);
+	}
+
+	IEnumerator Fade (float alphaValue)
+	{
+		Color currentColor = gameObject.renderer.material.color;
+		Color targetColor = gameObject.renderer.material.color;
+		targetColor.a = alphaValue;
+
+		// Fast-forward if the fade is midway through
+		float currentFadeProgress = Mathf.Abs (alphaValue - currentColor.a);
+		float elapsed = Mathf.Max (0, fadeTime - (currentFadeProgress * fadeTime));
+		while (elapsed < fadeTime) {
+			elapsed += Time.deltaTime;
+			gameObject.renderer.material.color = Color.Lerp (currentColor, targetColor, elapsed / fadeTime);
+			yield return null;
+		}
+	}
+
+}
